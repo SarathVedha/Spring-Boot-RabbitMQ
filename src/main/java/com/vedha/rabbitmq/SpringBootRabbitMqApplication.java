a88041bb3d77b64fe8ec1128c1bf552a5cf101d9
Sarath P <sarath1242000@gmail.com>
2023-09-07 19:19:22 +0530

    Spring Boot Rabbit MQ Demo

--- src/main/java/com/vedha/rabbitmq/SpringBootRabbitMqApplication.java
+++ src/main/java/com/vedha/rabbitmq/SpringBootRabbitMqApplication.java
@@ -1,9 +1,12 @@
 package com.vedha.rabbitmq;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Spring Boot RabbitMQ App", description = "RabbitMQ Demo"))
 public class SpringBootRabbitMqApplication {
 
 	public static void main(String[] args) {
