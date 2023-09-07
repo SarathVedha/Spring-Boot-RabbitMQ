f3b5b48a3780daa9d28baecbc4d58595792ece1e
Sarath P <sarath1242000@gmail.com>
2023-09-07 19:33:00 +0530

    Spring Boot Rabbit MQ Demo

--- src/main/java/com/vedha/rabbitmq/controller/SendMessage.java
+++ src/main/java/com/vedha/rabbitmq/controller/SendMessage.java
@@ -3,6 +3,9 @@ package com.vedha.rabbitmq.controller;
 import com.vedha.rabbitmq.dto.UserDTO;
 import com.vedha.rabbitmq.producer.RabbitMQJsonProducer;
 import com.vedha.rabbitmq.producer.RabbitMQProducer;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +14,15 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/rabbit")
 @AllArgsConstructor
+@Tag(name = "Message Controller", description = "Send Message To RabbitMQ")
 public class SendMessage {
 
     private final RabbitMQProducer rabbitMQProducer;
 
     private final RabbitMQJsonProducer rabbitMQJsonProducer;
 
+    @Operation(summary = "Send String Message", description = "Send String Message To RabbitMQ")
+    @ApiResponse(responseCode = "200", description = "Http Status 200 Ok")
     @GetMapping("/v1/sendMessage")
     public ResponseEntity<String> sendMessage(@RequestParam("message") String message) {
 
@@ -25,6 +31,8 @@ public class SendMessage {
         return ResponseEntity.ok("Message Sent To Queue vedha-queue : " + message);
     }
 
+    @Operation(summary = "Send Json Message", description = "Send Json Message To RabbitMQ")
+    @ApiResponse(responseCode = "200", description = "Http Status 200 Ok")
     @PostMapping(value = { "/v1/sendJsonMessage" }, consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = { MediaType.TEXT_PLAIN_VALUE })
     public ResponseEntity<String> sendJsonMessage(@RequestBody UserDTO userDTO) {
 
